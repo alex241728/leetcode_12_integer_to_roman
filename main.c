07cec18d6416f8896388cd209d475be545d70b53
alex241728 <alex_241728@163.com>
2024-02-26 10:55:52 -0500

    final commit

--- main.c
+++ main.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define MAX_STR_LEN 32
+#define MAX_STR_LEN (32)
 
-void intToRoman(char*, int);
+char* intToRoman(int);
 
 int main() {
-  char result[MAX_STR_LEN];
+  printf("%s\n", intToRoman(3));
 
-  intToRoman(result, 3);
-  printf("%s\n", result);
+  printf("%s\n", intToRoman(58));
 
-  intToRoman(result, 58);
-  printf("%s\n", result);
+  printf("%s\n", intToRoman(1994));
 
-  intToRoman(result, 1994);
-  printf("%s\n", result);
+  printf("%s\n", intToRoman(88));
 
   return 0;
 }
 
-void intToRoman(char* result, int num) {
+char* intToRoman(int num) {
     const char* M[] = {"", "M", "MM", "MMM"}; // store {empty, 1000, 2000, 3000}
     const char* C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC","DCCC",
-                       "DM"}; // stores {empty, 100, 200, 300, 400, 500, 600, 700, 800
-    const char* X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXX",
+                       "CM"}; // stores {empty, 100, 200, 300, 400, 500, 600, 700, 800
+    const char* X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX",
                        "XC"}; // stores {empty, 10, 20, 30, 40, 50, 60, 70, 80, 90}
     const char* I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII",
                        "IX"}; // stores {empty, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 
+    char* result = malloc(sizeof(char) * MAX_STR_LEN);
     sprintf(result, "%s%s%s%s", M[num / 1000], C[(num / 100) % 10], X[(num / 10) % 10], I[num % 10]);
+    return result;
 }
